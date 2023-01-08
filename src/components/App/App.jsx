4292caee6ba0687c5dbf43f70a23eb641c18e85f
Kirill Begey <kirill.begey@mail.ru>
2023-01-08 17:58:09 +0300

    fix: Подключение стилей css к компоненту App с помощью css-модулей. Классовый компонент App переделан в функциональный

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.css';
 import AppHeader from '../AppHeader/AppHeader';
 import BurgerIngredients from '../BurgerIngredients/BurgerIngredients';
 import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
 import { data } from '../../utils/data';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <>
-        <AppHeader/>
-        <main className="content">
-          <div className="container">
-            <BurgerIngredients data={data}/>
-            <BurgerConstructor data={data}/>
-          </div>
-        </main>
-      </>
-    );
-  }
+export default function App() {
+  return (
+    <>
+      <AppHeader />
+      <main className={styles.content}>
+        <div className={styles.container}>
+          <BurgerIngredients data={data} />
+          <BurgerConstructor data={data} />
+        </div>
+      </main>
+    </>
+  )
 }
-
-export default App;
