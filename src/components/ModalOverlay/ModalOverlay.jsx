8baf86d1bffd6591337463f4eb7cd8312cadfd19
Kirill-Begej <fleki679@yandex.ru>
2023-01-15 21:07:39 +0300

    fix: в компоненте ModalOverlay убрана лишняя обертка

--- src/components/ModalOverlay/ModalOverlay.jsx
+++ src/components/ModalOverlay/ModalOverlay.jsx
@@ -7,9 +7,7 @@ export const ModalOverlay = ({ onClose }) => {
   };
 
   return (
-    <>
-      <div className={styles.overlay} id="overlay" onClick={onCloseOverlay} />
-    </>
+    <div className={styles.overlay} id="overlay" onClick={onCloseOverlay} />
   );
 };
 
